facbf0b3a59869fa7d73b6a782c8cf7d02ace0df
xircon <xircon@users.noreply.github.com>
2026-04-11 13:24:29 +0100

    PKGBUILD fix

--- gitup.sh
+++ gitup.sh
@@ -3,6 +3,6 @@
 cd ~/scripts/tui-ss
 git status -sb
 git add -A
-git commit -m "Refine spreadsheet editing workflow"
+git commit -m "$1"
 GIT_SSH_COMMAND='ssh -F /dev/null -o IdentitiesOnly=yes -i /home/xircon/.ssh/id_rsa -o StrictHostKeyChecking=accept-new' git push -u origin main
 echo $(date)
